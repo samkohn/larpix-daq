9cc3485dd73b2626a1ba2a0d20a72dd518efb84a
Sam Kohn <kohn@berkeley.edu>
2018-12-26 18:11:13 -0800

    Add producer script. Doesn't work yet in Python 2

---
+++ larpixdaq/producer.py
@@ -0,0 +1,20 @@
+from moddaq import Producer
+import larpix.larpix as larpix
+
+try:
+    board = larpix.Controller()
+    board._serial._keep_open = True
+    producer = Producer('LArPix board', 'BOARD', 'producer-aggregator')
+    state = b''
+    producer.request_state()
+    while True:
+        producer.receive(0)
+        if state != producer.state:
+            print('State update: New state: %s' % producer.state)
+            state = producer.state
+        if state == b'RUN':
+            board.run(0.5, 'run')
+            packets = b''.join(p.bytes() for p in board.reads[-1])
+            producer.produce(packets)
+finally:
+    producer.cleanup()
