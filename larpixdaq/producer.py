dcfc98800eb1d8b22c4520087cfea87de3f832ba
Sam Kohn <kohn@berkeley.edu>
2019-01-09 12:48:16 -0800

    Update producer with debug messages

--- larpixdaq/producer.py
+++ larpixdaq/producer.py
@@ -3,7 +3,6 @@ from __future__ import print_function
 import argparse
 import logging
 import time
-logging.basicConfig(level=logging.DEBUG)
 from moddaq import Producer
 import larpix.larpix as larpix
 
@@ -11,7 +10,10 @@ try:
     parser = argparse.ArgumentParser()
     parser.add_argument('address')
     parser.add_argument('--core', default='tcp://127.0.0.1')
+    parser.add_argument('-d', '--debug')
     args = parser.parse_args()
+    if args.debug:
+        logging.basicConfig(level=logging.DEBUG)
     address = args.address
     core_url = args.core
     core_address = core_url + ':5550'
@@ -26,12 +28,14 @@ try:
     state = ''
     producer.request_state()
     while True:
-        producer.receive(1)
+        producer.receive(0.4)
         if state != producer.state:
             print('State update: New state: %s' % producer.state)
             state = producer.state
         if state == b'RUN':
+            logging.debug('about to run')
             board.run(0.5, 'run')
+            logging.debug('just took data')
             packets = b''.join(p.bytes() for p in board.reads[-1])
             metadata = {'name': 'LArPix board', 'timestamp':
                     time.time()}
