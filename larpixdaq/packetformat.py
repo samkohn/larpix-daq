40dd979366ae369adc772e58b71be154f7c0b9c1
Sam Kohn <kohn@berkeley.edu>
2019-02-28 14:57:54 -0800

    Add version number to packet format

--- larpixdaq/packetformat.py
+++ larpixdaq/packetformat.py
@@ -5,9 +5,12 @@ Formatter for LArPix data packets + medatadata to/from bytestreams.
 from larpix.larpix import Packet
 
 def toBytes(packets):
-    return b'\xAA'.join(p.bytes() for p in packets)
+    return b'0.1/' + b'\xAA'.join(p.bytes() for p in packets)
 
 def fromBytes(bytestream):
-    split = [bytestream[n:n+8] for n in range(0, len(bytestream), 8)]
+    version, slash, stream = bytestream.partition(b'/')
+    if slash == b'':
+        raise ValueError('No version found')
+    split = [stream[n:n+8] for n in range(0, len(stream), 8)]
     packets = [Packet(x[:7]) for x in split]
     return packets
