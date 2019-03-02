22ea354e49e348b1720fb818d4836bad731c20bb
Sam Kohn <kohn@berkeley.edu>
2019-03-01 17:38:55 -0800

    Connect to Core via Operator objects as if it were a database
    Code structure taken from Flask tutorial

--- webapp/app.py
+++ webapp/app.py
@@ -6,14 +6,10 @@ import json
 
 from flask import Flask, render_template, current_app
 
-from larpixdaq.operator import Operator
+from .daq import get_daq
 
 def create_app():
     app = Flask(__name__)
-
-    @app.before_first_request
-    def init_operator():
-        app.config['OPERATOR'] = Operator()
 
     @app.route('/')
     def hello():
@@ -21,28 +17,34 @@ def create_app():
 
     @app.route('/command/start-run', methods=['POST'])
     def start_run():
-        current_app.config['OPERATOR'].begin_physics_run()
+        daq = get_daq()
+        action_id = daq.begin_physics_run()
+        print(action_id)
         return 'nothing'
 
     @app.route('/command/end-run', methods=['POST'])
     def end_run():
-        actionid = current_app.config['OPERATOR'].end_physics_run()
-        return str(actionid)
+        daq = get_daq()
+        daq.end_physics_run()
+        return 'nothing'
 
     @app.route('/command/process', methods=['POST'])
     def process_messages():
-        current_app.config['OPERATOR'].process_incoming_messages()
+        daq = get_daq()
+        daq.process_incoming_messages()
         return 'nothing'
 
     @app.route('/command/actionid/<actionid>')
     def get_action_id(actionid):
-        o = current_app.config['OPERATOR']
+        o = get_daq()
         try:
             result = o.retrieve_result(int(actionid))
         except:
             result = 'invalid id'
         return json.dumps(result)
 
+    from . import daq
+    daq.init_app(app)
 
     return app
 
