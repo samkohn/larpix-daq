5194d216a7a620785cc9cca7ebe292728199863d
Sam Kohn <kohn@berkeley.edu>
2019-03-06 12:02:22 -0800

    Remove old jQuery code

--- webapp/static/js/main.js
+++ webapp/static/js/main.js
@@ -1,34 +1,6 @@
 'use strict';
 
 var socket = io();
-$(document).ready(function() {
-  console.log(socket);
-  socket.on('/', function(msg) {
-    console.log(msg);
-  });
-  socket.emit('', 'World');
-  $('#start-run').click(function() {
-    socket.emit('command/start-run', '', function(msg) {
-      msg = JSON.stringify(JSON.parse(msg), null, 5);
-      console.log(msg);
-      $('body').append('<br>Run has begun! ' + msg);
-    });
-  });
-  $('#end-run').click(function() {
-    socket.emit('command/end-run', '', function(msg) {
-      $('body').append('<br>Run has ended! ' + msg);
-    });
-  });
-  $('#retrieve-action').click(function() {
-    $.get('/command/actionid/' + $('#retrieve-action-input').val(), function(strdata) {
-      console.log(strdata);
-      data = $.parseJSON(strdata);
-      console.log(data);
-      $('body').append('<br>' + data.result);
-    });
-  });
-});
-
 class ActionTrigger extends React.Component {
   constructor(props) {
     super(props);
