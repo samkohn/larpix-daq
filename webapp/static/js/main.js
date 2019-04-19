samkohn/larpix-daq

commit 233a17d4bdd8f9cf160c9e1c5e02158453c91297

Update setState calls to ensure that "state" arg is not modified

webapp/static/js/main.js
@@ -52,7 +52,8 @@ class ActionTrigger extends React.Component {
     const new_value = event.target.value;
     const index = Number(event.target.name);
     this.setState(function(state, props) {
-        state.input_values[index] = new_value;
+        let new_inputs = state.input_values.slice();
+        new_inputs[index] = new_value;
         return {input_values: state.input_values};
     });
   }
@@ -267,7 +268,7 @@ class ActionDashboard extends React.Component {
 
   onRegisterChange(name, newValue) {
     this.setState(function(state, props) {
-      const pane = state.configuration_pane;
+      const pane = {...state.configuration_pane};
       const chip = pane.chip;
       pane.values[chip][name] = newValue;
       return {configuration_pane: pane};
@@ -276,7 +277,7 @@ class ActionDashboard extends React.Component {
 
   onChipChange(newChip) {
     this.setState(function(state, props) {
-      const pane = state.configuration_pane;
+      const pane = {...state.configuration_pane};
       pane.chip = newChip;
       return {configuration_pane: pane};
     });
@@ -291,7 +292,11 @@ class ActionDashboard extends React.Component {
         result: '',
       },
     };
-    this.setState((state, props) => state.results.push(first_description));
+    this.setState((state, props) => {
+      let new_results = state.results.slice();
+      new_results.push(first_description);
+      return {results: new_results};
+    });
   }
   render() {
     const obj = this;

commit 2566b7129bd93b3a4e16956d5fc26cb3514d5008

Fix bug introduced in 233a17d

webapp/static/js/main.js
@@ -53,7 +53,7 @@ class ActionTrigger extends React.Component {
     this.setState(function(state, props) {
         let new_inputs = state.input_values.slice();
         new_inputs[index] = new_value;
-        return {input_values: state.input_values};
+        return {input_values: new_inputs};
     });
   }
 