b08f3d7cad5b050ce13488c8742a8627c460e102
Sam Kohn <kohn@berkeley.edu>
2019-05-22 17:15:54 -0700

    Update core.py to support the global log

--- larpixdaq/core.py
+++ larpixdaq/core.py
@@ -7,11 +7,13 @@ import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument('--address', default='tcp://127.0.0.1',
         help='base address for ZMQ connections')
+parser.add_argument('--log-address', default='tcp://127.0.0.1:5678',
+        help='Address to connect to global log')
 
 args = parser.parse_args()
 base_address = args.address + ':'
 
-core = Core(base_address + '5551')
+core = Core(base_address + '5551', args.log_address)
 
 allowed_states = ['', 'START', 'INIT', 'READY', 'RUN',
         'SUBROUTINE', 'STOP']
