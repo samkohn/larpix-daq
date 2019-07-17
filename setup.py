673160a4f2f1710573ac6c9a146df35e79b07893
Sam Kohn <kohn@berkeley.edu>
2019-07-16 17:39:27 -0700

    Update setup.py for launch, including VERSION file

--- setup.py
+++ setup.py
@@ -1,14 +1,33 @@
 from setuptools import setup, find_packages
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with open(os.path.join(here, 'README.md')) as f:
+    long_description = f.read()
+
+with open(os.path.join(here, 'VERSION')) as f:
+    version = f.read()
+
 
 setup(
-        name='larpixdaq',
-        version='0.0.1',
+        name='larpix-daq',
+        version=version,
         description='LArPix DAQ system',
-        long_description='A DAQ system for LArPix built on xylem',
-        author='Sam Kohn',
+        long_description=long_description,
+        long_description_content_type='text/markdown',
+        author='Sam Kohn, LBNL Neutrino Group',
         author_email='skohn@lbl.gov',
+        keywords='dune larpix',
+        classifiers=[
+            'Development Status :: 4 - Beta',
+            'Intended Audience :: Science/Research',
+            'Programming Language :: Python',
+            'Topic :: Scientific/Engineering :: Physics',
+            'Topic :: Scientific/Engineering',
+            ],
         packages=find_packages(),
         install_requires=['xylem-daq', 'flask >=1.0.0', 'flask-socketio >=3.0',
             'eventlet >= 0.24', 'requests ~= 2.18',
-            'larpix-control == 2.3.0rc1'],
+            'larpix-control ~= 2.3.0'],
 )
