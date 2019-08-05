d1b57d3714e968e746c668db324fa970dc9ebbea
Sam Kohn <kohn@berkeley.edu>
2019-08-05 13:15:02 -0700

    Remove old PCB numbers and add pcb-1.5

--- larpixdaq/producer.py
+++ larpixdaq/producer.py
@@ -207,12 +207,7 @@ class LArPixProducer(object):
         name.
         """
         boards = [
-                'pcb-1',
-                'pcb-2',
-                'pcb-3',
-                'pcb-4',
-                'pcb-5',
-                'pcb-6',
+                'pcb-1.5',
                 'pcb-10',
                 ]
         board_data = []
